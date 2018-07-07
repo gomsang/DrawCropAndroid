40f145eb690d5e212160aa3be7840d6ca34abdb7
Gyeongrok Kim <devkgl1005@gmail.com>
2018-07-07 21:50:44 +0900

    draw crop view constructed

---
+++ app/src/main/java/com/gomsang/drawcropandroid/MainActivity.java
@@ -0,0 +1,35 @@
+package com.gomsang.drawcropandroid;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.gomsang.drawcropandroid.libs.DrawCropView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final DrawCropView drawCropView = findViewById(R.id.cropView);
+        final ImageView imageView = findViewById(R.id.imageView);
+
+        final BitmapDrawable drawable = (BitmapDrawable) getResources().getDrawable(R.drawable.mango);
+        final Bitmap bitmap = drawable.getBitmap();
+
+        drawCropView.setImageBitmap(bitmap);
+        drawCropView.setOnCropListener(new DrawCropView.OnCropListener() {
+            @Override
+            public void onCrop(Bitmap result) {
+                imageView.bringToFront();
+                drawCropView.setVisibility(View.GONE);
+                imageView.setImageBitmap(result);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/gomsang/drawcropandroid/libs/DrawCropView.java
@@ -0,0 +1,236 @@
+package com.gomsang.drawcropandroid.libs;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.CornerPathEffect;
+import android.graphics.DashPathEffect;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class DrawCropView extends View implements View.OnTouchListener {
+    private final int DISTANCE_CONSIDER_CLOESR = 50;
+
+    private int canvasWidth, canvasHeight;
+
+    private Bitmap targetOriginalBitmap;
+    private Bitmap actualVisibleBitmap;
+
+    private ArrayList<Coordinate> drawCoordinates = new ArrayList<>();
+
+    private Paint linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    private OnCropListener onCropListener;
+
+
+    public DrawCropView(Context context) {
+        super(context);
+        init();
+    }
+
+    public DrawCropView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public DrawCropView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void init() {
+        // construct paint for line (draw path)
+        linePaint.setStyle(Paint.Style.STROKE);
+        linePaint.setPathEffect(new DashPathEffect(new float[]{10, 20}, 0));
+        linePaint.setStrokeWidth(5);
+        linePaint.setShadowLayer(10.0f, 0.0f, 2.0f, 0xFF000000);
+        linePaint.setColor(Color.WHITE);
+
+        drawCoordinates.clear();
+        setOnTouchListener(this);
+        invalidate();
+    }
+
+    public void setImageBitmap(Bitmap imageBitmap) {
+        targetOriginalBitmap = imageBitmap;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvasWidth = canvas.getWidth();
+        canvasHeight = canvas.getHeight();
+
+        if (targetOriginalBitmap != null) {
+            // If target's original bitmap is bigger than view size, adjust size for fit
+            actualVisibleBitmap = scaleBitmapAndKeepRation(targetOriginalBitmap, canvas.getHeight(), canvas.getWidth());
+            canvas.drawBitmap(actualVisibleBitmap, canvasWidth / 2 - actualVisibleBitmap.getWidth() / 2,
+                    canvasHeight / 2 - actualVisibleBitmap.getHeight() / 2, null);
+        } else {
+            Paint textPaint = new Paint();
+            canvas.drawPaint(textPaint);
+            textPaint.setColor(Color.BLACK);
+            textPaint.setTextSize(16);
+            canvas.drawText("Please set image bitmap for process", canvasWidth, canvasHeight, textPaint);
+        }
+
+        if (drawCoordinates.size() > 0) {
+            canvas.drawPath(generatePathByCoordinate(drawCoordinates, 1), linePaint);
+        }
+    }
+
+    public static Bitmap scaleBitmapAndKeepRation(Bitmap TargetBmp, int reqHeightInPixels, int reqWidthInPixels) {
+        Matrix m = new Matrix();
+        m.setRectToRect(new RectF(0, 0, TargetBmp.getWidth(), TargetBmp.getHeight()), new RectF(0, 0, reqWidthInPixels, reqHeightInPixels), Matrix.ScaleToFit.CENTER);
+        return Bitmap.createBitmap(TargetBmp, 0, 0, TargetBmp.getWidth(), TargetBmp.getHeight(), m, true);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        // 터치된 좌표를 데이터화 함
+        final Coordinate currentCoordinate = new Coordinate();
+        currentCoordinate.x = (int) event.getX();
+        currentCoordinate.y = (int) event.getY();
+        adjustCoordinateForFit(currentCoordinate);
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                drawCoordinates.add(currentCoordinate);
+                invalidate();
+                if (drawCoordinates.size() < 24) break;
+                this.invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                if (!(measureDistance(drawCoordinates.get(0), currentCoordinate) < DISTANCE_CONSIDER_CLOESR)) {
+                    Toast.makeText(getContext(), "Please put up your hands in closer with first position", Toast.LENGTH_SHORT).show();
+                    drawCoordinates.clear();
+                }else{
+                    showProduceDialog();
+                }
+                // 뷰동기화
+                this.invalidate();
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+    // generate path by coordinates arr
+    private Path generatePathByCoordinate(ArrayList<Coordinate> coordinates, int interval) {
+        Path path = new Path();
+        boolean first = true;
+        for (int ci = 0; ci < coordinates.size(); ci += interval) {
+            Coordinate coordinateData = coordinates.get(ci);
+            if (first) {
+                first = false;
+                path.moveTo(coordinateData.x, coordinateData.y);
+            } else if (ci < coordinates.size() - 1) {
+                Coordinate next = coordinates.get(ci + 1);
+                path.quadTo(coordinateData.x, coordinateData.y, next.x, next.y);
+            } else {
+                coordinateData = coordinates.get(ci);
+                path.lineTo(coordinateData.x, coordinateData.y);
+            }
+        }
+        return path;
+    }
+
+    private int measureDistance(Coordinate targetA, Coordinate targetB) {
+        double distance = Math.sqrt(Math.pow(targetA.x - targetB.x, 2) + Math.pow(targetA.y - targetB.y, 2));
+        return (int) distance;
+    }
+
+    private Coordinate adjustCoordinateForFit(Coordinate targetCoordinate) {
+        // if selected coordinate is over the actual visible bitmap's area, adjust it.
+        final int targetWidth = actualVisibleBitmap.getWidth();
+        final int targetHeight = actualVisibleBitmap.getHeight();
+        final int targetStartWidth = (canvasWidth - targetWidth) / 2;
+        final int targetStartHeight = (canvasHeight - targetHeight) / 2;
+
+        if (targetCoordinate.x < targetStartWidth)
+            targetCoordinate.x = targetStartWidth;
+        if (targetCoordinate.x > targetStartWidth + targetWidth)
+            targetCoordinate.x = targetStartWidth + targetWidth;
+        if (targetCoordinate.y < targetStartHeight)
+            targetCoordinate.y = targetStartHeight;
+        if (targetCoordinate.y > targetStartHeight + targetHeight)
+            targetCoordinate.y = targetStartHeight + targetHeight;
+
+        return targetCoordinate;
+    }
+
+    private void showProduceDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Do you proceed like this?");
+        builder.setMessage("The image will be generated as you draw.");
+        builder.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (onCropListener != null) onCropListener.onCrop(produce());
+                        init();
+                    }
+                });
+        builder.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        init();
+                    }
+                });
+        builder.show();
+    }
+
+    public void setOnCropListener(OnCropListener onCropListener) {
+        this.onCropListener = onCropListener;
+    }
+
+    public Bitmap produce() {
+        Bitmap resultImage = Bitmap.createBitmap(canvasWidth, canvasHeight, actualVisibleBitmap.getConfig());
+
+        final Canvas resultCanvas = new Canvas(resultImage);
+        final Paint resultPaint = new Paint();
+
+        // struct paint for naturally
+        resultPaint.setAntiAlias(true);
+        resultPaint.setDither(true);                    // set the dither to true
+        resultPaint.setStrokeJoin(Paint.Join.ROUND);    // set the join to round you want
+        resultPaint.setStrokeCap(Paint.Cap.ROUND);      // set the paint cap to round too
+        resultPaint.setPathEffect(new CornerPathEffect(10));
+        resultPaint.setAntiAlias(true);                         // set anti alias so it smooths
+
+        // struct paint for path-crop
+        resultCanvas.drawPath(generatePathByCoordinate(drawCoordinates, 1), resultPaint);
+        resultPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+
+        resultCanvas.drawBitmap(actualVisibleBitmap, canvasWidth / 2 - actualVisibleBitmap.getWidth() / 2,
+                canvasHeight / 2 - actualVisibleBitmap.getHeight() / 2, resultPaint);
+
+        return BitmapUtil.cropBitmapToBoundingBox(resultImage, Color.TRANSPARENT);
+    }
+
+    public class Coordinate {
+        float x, y;
+    }
+
+    public interface OnCropListener {
+        void onCrop(Bitmap result);
+    }
+}
